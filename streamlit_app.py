d33e8c3dee5dc798447a6fee6b95b41a341037bf
UdinHeker <121849343+Arippp9@users.noreply.github.com>
2026-06-01 22:26:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -489,8 +489,7 @@ st.set_page_config(
 # ==========================================
 # 1. DATASET KIMIA (PRESETS ZAT & INDIKATOR)
 # ==========================================
-if page == ""
-elif page == "Simulasi":
+if page == "Simulasi":
     st.title("Coba simulasi ini")
 CHEMICALS = [
     {"id": "hcl", "name": "Asam Klorida (HCl)", "formula": "HCl", "pH": 1.0, "type": "asam", "category": "Laboratorium", "common": "Asam kuat pembersih porselen", "dissociation": "HCl → H⁺ + Cl⁻"},
